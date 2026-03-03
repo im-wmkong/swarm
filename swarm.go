2df9ef483c0fbb3b79b5685111612c8367ec0c41
Weimin Kong <im.wmkong@gmail.com>
2026-03-03 02:51:43 +0000

    move options

--- swarm.go
+++ swarm.go
@@ -9,63 +9,6 @@ import (
 	"sync"
 )
 
-// Options 定义了并发执行的各种配置
-type Options struct {
-	concurrency  int
-	ctx          context.Context
-	failFast     bool
-	panicHandler func(p any) error
-	retryTimes   int
-}
-
-// Option 定义了修改 Options 的函数签名
-type Option func(*Options)
-
-// WithConcurrency 设置最大并发量。如果不设置，默认使用当前机器的 CPU 核心数。
-func WithConcurrency(n int) Option {
-	return func(o *Options) {
-		if n > 0 {
-			o.concurrency = n
-		}
-	}
-}
-
-// WithContext 注入上下文，可用于超时控制或外部主动取消任务。
-func WithContext(ctx context.Context) Option {
-	return func(o *Options) {
-		if ctx != nil {
-			o.ctx = ctx
-		}
-	}
-}
-
-// WithFailFast 开启快速失败模式。任意一个任务失败，将不再调度后续未开始的任务。
-func WithFailFast() Option {
-	return func(o *Options) {
-		o.failFast = true
-	}
-}
-
-// WithPanicHandler 自定义 Panic 捕获和处理逻辑。
-// 如果传入 nil，则忽略此选项，继续使用默认的处理逻辑（记录堆栈并返回 error）。
-// 如果想要完全吞噬或忽略 Panic 错误，请传入一个返回 nil 的函数：func(p any) error { return nil }
-func WithPanicHandler(handler func(p any) error) Option {
-	return func(o *Options) {
-		if handler != nil {
-			o.panicHandler = handler
-		}
-	}
-}
-
-// WithRetry 设置单个任务失败时的重试次数（不包含首次执行）。
-func WithRetry(times int) Option {
-	return func(o *Options) {
-		if times > 0 {
-			o.retryTimes = times
-		}
-	}
-}
-
 // Run 并发地对数据切片中的每个项执行 handle 函数。
 func Run[T any](data []T, handle func(item T) error, opts ...Option) error {
 	if len(data) == 0 {
